2df1084097090137c2d9d4e609074a34a66ef004
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-12-03 07:32:46 -0500

    Added some demo code

---
+++ StringStuff/src/strings/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * Demonstrating the String and StringBuilder data types
+ */
+package strings;
+public class Main {
+
+	public static void main(String[] args) {
+		testSomeStrings();
+		testSomeStringBuilders();
+	}
+	public static void testSomeStrings() {
+		String test = "Todd Lindemann";
+		long startTime = System.nanoTime();
+		for (int i = 0; i < 10000000; i++) {
+			test = "Steve Hart";
+			test = "Michael Lewis";
+		}
+		long endTime = System.nanoTime();
+
+		// get difference of two nanoTime values
+		long timeElapsed = endTime - startTime;
+		System.out.println("testSomeStrings(): Execution time in milliseconds : " + (double)timeElapsed / 1000000);
+	}
+	/***
+	 * Run some StringBuilder operations and see how long it takes
+	 */
+	public static void testSomeStringBuilders() {
+		StringBuilder test = new StringBuilder("Todd Lindemann");
+		long startTime = System.nanoTime();
+		for (int i = 0; i < 10000000; i++) {
+			test.replace(1, 100, "Steve Hart");
+			test.replace(1, 100, "Michael Lewis");
+		}
+		long endTime = System.nanoTime();
+
+		// get difference of two nanoTime values
+		long timeElapsed = endTime - startTime;
+		System.out.println("testSomeStringBuilders(): Execution time in milliseconds : " + (double)timeElapsed / 1000000);
+	}
+
+}
